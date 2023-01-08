aded03c6be9985d394df4a2d3d7ded305ea49ac0
Ashish Kumar Sahoo <111702590+ashish4kmax@users.noreply.github.com>
2023-01-08 22:53:19 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 4
+
+void push_back(int DEQ[], int *rear, int *front, int *count) {
+    int elem;
+    if(*count==MAX) {
+        printf("Dequeue is full\n");
+        return;
+    }
+    printf("Enter the element : ");
+    scanf("%d",&elem);
+    *rear=(*rear+1)%MAX;
+    *count=*count+1;
+    DEQ[*rear]=elem;
+    if(*front=-1) {
+        *front=*front+1;
+    }
+}
+
+void pop_back(int DEQ[], int *rear, int *front, int *count) {
+    if(*count==-1) {
+        printf("Dequeue underflow\n");
+        return;
+    }
+    if(*rear==-1) {
+        *rear=MAX-1;
+    }
+    printf("The deleted element is = %d\n",DEQ[*rear]);
+    *rear=*rear-1;
+     *count=*count-1;
+}
+
+void push_front(int DEQ[], int *rear, int *front, int *count) {
+    int elem;
+    if(*count==MAX) {
+        printf("Dequeue is full\n");
+    }
+    else {
+        printf("Enter the element : ");
+        scanf("%d", &elem);
+        if(*front==-1) {
+            *front=MAX-1;
+        }
+        else {
+            *front = *front - 1;
+        }
+        DEQ[*front] = elem;
+        *count = *count + 1;
+    }
+}
+
+void pop_front(int DEQ[], int *rear, int *front, int *count) {
+    if(*count==0) {
+        printf("Dequeue underflow\n");
+        return;
+    }
+    printf("The deleted element is = %d\n",DEQ[*front]);
+    *front=(*front+1)%MAX;
+    *count=*count-1;
+}
+
+void display(int DEQ[], int *rear, int* front, int *count) {
+    if(*count==0) {
+        printf("Dequeue is empty\n");
+        return;
+    }
+    int i;
+    printf("The dequeue elements are :\n");
+    for(i=*front;i<=*rear;i++) {
+        printf("%d\n",DEQ[i]);
+    }
+}
+
+int main() {
+    int DEQ[MAX], front=-1, rear=-1, choice;
+    int count=0;
+    for(;;) {
+        printf("Menue\n");
+        printf("1.Insert an element at front of queue\n");
+        printf("2.Delete an element at the front of queue\n");
+        printf("3.Insert an element at the back of queue\n");
+        printf("4.Delete an element at the back of queue\n");
+        printf("5.Display the dequeue elements\n");
+        printf("6.Exit\n");
+        printf("Enter your choice : ");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            push_front(DEQ,&rear,&front,&count);
+            break;
+
+        case 2:
+            pop_front(DEQ,&rear,&front,&count);
+            break;
+
+        case 3:
+            push_back(DEQ,&rear,&front,&count);
+            printf("%d\n",rear);
+            break;
+
+        case 4:
+            pop_back(DEQ,&rear,&front,&count);
+            break;
+
+        case 5:
+            display(DEQ,&rear,&front,&count);
+            break;
+
+        case 6:
+            exit(0);
+            break;           
+
+        default:
+            printf("Invalid Choice!!\n");
+            break;
+        }
+    }
+    return 0;
+}
---
+++ Q7.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node
+{
+    char usn[12];
+    char name[25];
+    char branch[5];
+    int sem;
+    float phone;
+    struct node *next;
+};
+typedef struct node *NODE;
+
+NODE getnode()
+{
+    return ((struct node *)malloc(sizeof(struct node)));
+}
+
+NODE insertbeg(NODE head)
+{
+    char usn[12], name[25], branch[5];
+    int sem;
+    float phone;
+    printf("Enter the students USN : ");
+    scanf("%s", usn);
+    getchar();
+    printf("Enter the name of the student : ");
+    scanf("%s", name);
+    getchar();
+    printf("Enter the branch of student : ");
+    scanf("%s", branch);
+    printf("Enter the students semester in : ");
+    scanf("%d", &sem);
+    printf("Enter the phone number of student : ");
+    scanf("%f", &phone);
+    NODE newnode = getnode();
+    strcpy(newnode->usn, usn);
+    strcpy(newnode->name, name);
+    strcpy(newnode->branch, branch);
+    newnode->sem = sem;
+    newnode->phone = phone;
+    newnode->next = head;
+}
+
+NODE deletefront(NODE head)
+{
+    if (head == NULL)
+    {
+        printf("Liked List is empty!!\n");
+    }
+    else if (head->next == NULL)
+    {
+        head = NULL;
+        return head;
+    }
+    else
+    {
+        NODE temp = head->next;
+        printf("Deleted data is of Name = %s\n", head->name);
+        free(head);
+        return temp;
+    }
+}
+
+NODE deleteend(NODE head)
+{
+    if (head == NULL)
+    {
+        printf("Linked List is empty!!\n");
+    }
+    else
+    {
+        NODE curr = head, prev;
+        while (curr->next != NULL)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        prev->next = NULL;
+        printf("Deleted data is of Name = %s\n", curr->name);
+        free(curr);
+        return head;
+    }
+}
+
+void display(NODE head)
+{
+    int count = 0;
+    if (head == NULL)
+    {
+        printf("Linked list is empty!!\n");
+    }
+    else
+    {
+        NODE curr = head;
+        printf("USN\t\tName\t\tBranch\tSem\tPhone\n");
+        while (curr != NULL)
+        {
+            count++;
+            printf("%-20s%-15s%-5s%5d%15.0f\n", curr->usn, curr->name, curr->branch, curr->sem, curr->phone);
+            curr = curr->next;
+        }
+        printf("\nThe no. of nodes = %d\n", count);
+    }
+}
+
+int main()
+{
+    NODE head = NULL;
+    int choice;
+    int n, i;
+    printf("Enter the amount of student data you want to insert : ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        head = insertbeg(head);
+    }
+    
+    for (;;)
+    {
+        printf("Menue\n");
+        printf("1.Insert the student datas\n");
+        printf("2.Delete data from front\n");
+        printf("3.Delete data from the end\n");
+        printf("4.Display the students data\n");
+        printf("5.Exit\n");
+        printf("Enter the choice : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            head = insertbeg(head);
+            break;
+
+        case 2:
+            head = deletefront(head);
+            break;
+
+        case 3:
+            head = deleteend(head);
+            break;
+
+        case 4:
+            display(head);
+            break;
+
+        default:
+            exit(0);
+        }
+    }
+    return 0;
+}
